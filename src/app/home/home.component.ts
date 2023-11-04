9e956770ab3cce37e14b63da7dc65c6cc33ee12c
camilorojase <c.rojase@uniandes.edu.co>
2023-11-04 11:57:02 -0500

    Add new css to Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../shared/shared.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  constructor(public sharedService: SharedService) {
+    this.sharedService.setSite('Home');
+  }
 
 }
