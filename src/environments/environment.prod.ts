87a280af5f892b6b89440e210410caf3edf515b7
Camilo <dc.perezc1@uniandes.edu.co>
2023-11-09 23:57:34 -0500

    quitar proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,18 @@
 const baseUrl: string = 'http://34.117.27.162/';
+const baseUrlUsers: string = 'http://34.117.27.162/users/';
+const baseUrlEvaluations: string = 'http://34.117.27.162/evaluations/';
+const baseUrlProjects: string = 'http://34.117.27.162/projects/';
+const baseUrlInterviews: string = 'http://34.117.27.162/interviews/';
+const baseUrlMediator: string = 'http://34.117.27.162/mediator/';
+
 export const environment = {
   production: true,
   baseUrl,
-  sur: 'sur'
+  baseUrlEvaluations,
+  baseUrlUsers,
+  baseUrlProjects,
+  baseUrlInterviews,
+  baseUrlMediator,
+  sur: 'sur',
+  token: 'token'
 };
