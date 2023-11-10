f86630d2bc449eeabaec40b648924f3a26aad484
jhinariv <jhina.riv@gmail.com>
2023-11-09 22:20:43 -0500

    Segunda version eliminar recurso tecnico

--- src/app/user/technical-resource/technical-resource-delete/technical-resource-delete.component.ts
+++ src/app/user/technical-resource/technical-resource-delete/technical-resource-delete.component.ts
@@ -1,27 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, FormArray, UntypedFormArray } from '@angular/forms';
-import { ActivatedRoute, NavigationStart, NavigationExtras, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { ToastrService } from 'ngx-toastr';
 import { UserSessionDto } from 'src/app/shared/model/user-session';
-import { TechnicalResource } from 'src/app/shared/model/technical-resource';
 import { TechnicalResourceService } from '../technical-resource.service';
 import { UserSessionService } from 'src/app/shared/user-session/user-session.service';
-import { Location } from '@angular/common';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { AcademicInformationComponent } from '../../academic-information/academic-information.component';
-import { AcademicInformation } from 'src/app/shared/model/academic-information';
-import { ProfessionalExperiences } from 'src/app/shared/model/professional-experience';
-import { Language } from 'src/app/shared/model/language';
-import { PersonalSkill } from 'src/app/shared/model/personal-skill';
-import { ProgrammingLanguages } from 'src/app/shared/model/programming-language';
-import { ProfessionalExperienceComponent } from '../../professional-experience/professional-experience.component';
-import { ProgrammingLanguageComponent } from '../../programming-language/programming-language.component';
-import { LanguageComponent } from '../../language/language.component';
-import { PersonalSkillComponent } from '../../personal-skill/personal-skill.component';
-import { SharedService } from 'src/app/shared/shared.service';
 
 @Component({
   selector: 'app-technical-resource-delete',
@@ -32,17 +16,11 @@ export class TechnicalResourceDeleteComponent {
   @Input() userSessionDto!: UserSessionDto;
 
   helper = new JwtHelperService();
-  userForm: FormGroup;
+  userDeleteForm: FormGroup;
   token: string | null;
-  carga: boolean = false;
-  user: TechnicalResource;
-  myUser: UserSessionDto;
-  userSessionForm: FormGroup;
   localStageData: any;
-  genres: any = [];
   closeResult = '';
   url!: string;
-  datos: any;
   userId: number;
 
   constructor(
@@ -51,12 +29,11 @@ export class TechnicalResourceDeleteComponent {
     private formBuilder: FormBuilder,
     private router: Router,
     private toastr: ToastrService,
-    private sharedService: SharedService,
     private route: ActivatedRoute
   ) { }
 
   createForm() {
-    this.userForm = this.formBuilder.group({
+    this.userDeleteForm = this.formBuilder.group({
       aceptTerms: ['', [Validators.required]],
       username: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
@@ -69,7 +46,6 @@ export class TechnicalResourceDeleteComponent {
     this.createForm();
     this.token = localStorage.getItem('token');
     if (this.token) {
-      const decodedToken = this.helper.decodeToken(this.token);
       this.userSessionDto = this.localStageData?.data as UserSessionDto;
       this.router.navigate([`/technical-resource/delete/` + userId]);
     } else {
@@ -78,38 +54,40 @@ export class TechnicalResourceDeleteComponent {
   }
 
   async deleteUser() {
-    let userAux = { ...this.userForm.value };
+    let userAux = { ...this.userDeleteForm.value };
     console.log(this)
     this.token = localStorage.getItem('token');
     if (this.token) {
       const decodedToken = this.helper.decodeToken(this.token);
-      console.log(localStorage)
-      console.log(this)
-      console.log(userAux)
-
-
-      if (this.userSessionDto.username == userAux.username && this.userSessionDto.username == userAux.password) {
-        this.userService.deleteUser(decodedToken.sub).subscribe({
-          next: (response: any) => {
-            this.userSessionService.sendMessage(true);
-            this.userSessionService.saveUserLocal(response);
-            this.toastr.success(`delete succesful`, 'Success', {
+      this.userSessionService.getUserMe().subscribe({
+        next: (response: any) => {
+          if (response.username == userAux.username) {
+            this.userService.deleteUser(decodedToken.sub).subscribe({
+              next: (response: any) => {
+                this.userSessionService.sendMessage(true);
+                this.userSessionService.saveUserLocal(response);
+                this.toastr.success(`delete succesful`, 'Success', {
+                  progressBar: true,
+                });
+                this.router.navigate([`/home`]);
+              },
+              error: (e0: any) => {
+                this.toastr.error(`delete fail`, 'Error, ' + e0, {
+                  progressBar: true,
+                });
+              },
+              complete: () => {
+                console.log('delete0')
+                this.userSessionService.closeSession();
+              },
+            });
+          } else {
+            this.toastr.error(`Username is incorrect`, 'Error', {
               progressBar: true,
             });
-            this.router.navigate([`/home`]);
-          },
-          error: (e0: any) => {
-            this.toastr.error(`delete fail`, 'Error, ' + e0, {
-              progressBar: true,
-            });
-          },
-          complete: () => console.log('delete0'),
-        });
-      } else {
-        this.toastr.error(`Username or password are invalid`, 'Error', {
-          progressBar: true,
-        });
-      }
+          }
+        }
+      });
     }
 
   }
